0e3c6b501ec40149054e516cd58fbf1964c6d404
Yoan Gaudiero <yoan.gaudiero@epitech.eu>
2024-04-17 19:07:04 +0200

    fix: size of text for "Powered By"

--- src/loading_screen/loading.c
+++ src/loading_screen/loading.c
@@ -17,12 +17,12 @@ void start_load_one(game_data_t *game)
     sprite_id_t elements[] = {SP_LOADING_1, SP_FLAVIBOT, SP_ORA, SP_TRYADE};
     int element_count = 4;
     sfText *text;
-    sfVector2f text_pos = {700, 200};
+    sfVector2f text_pos = {750, 200};
 
     for (size_t i = 0; i < element_count; i++) {
         sfRenderWindow_drawSprite(game->window, get_sprite(game, elements[i]), NULL);
     }
-    text = set_text(game, "POWERED BY", 38, text_pos);
+    text = set_text(game, "POWERED BY", 30, text_pos);
     sfText_setLetterSpacing(text, 10.0f);
     sfText_setStyle(text, sfTextBold);
     sfRenderWindow_drawText(game->window, text, NULL);
