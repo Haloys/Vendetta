226c144f96b4cf5b881d8e53abec5781ea04437c
flav-code <flavien.de-rigne@epitech.eu>
2024-05-20 08:49:58 +0200

    feat: reset view size in menu

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -9,5 +9,11 @@
 
 void change_game_mode(game_data_t *game, state_t new_mode)
 {
+    if (game->state != new_mode) {
+        printf("Changing mode to %d\n", new_mode);
+        sfView_setCenter(game->menu_view, (sfVector2f){WINDOW_WIDTH / 2,
+            WINDOW_HEIGHT / 2});
+        sfRenderWindow_setView(game->window, game->menu_view);
+    }
     game->state = new_mode;
 }
