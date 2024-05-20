11c96c2e4c40b211ca17a8ff7d68ac1d84bd142e
flav-code <flavien.de-rigne@epitech.eu>
2024-05-20 06:30:54 +0200

    fix: crash when leaving map

--- src/gameplay/utils.c
+++ src/gameplay/utils.c
@@ -9,5 +9,7 @@
 
 sfColor get_pixel_color(sfImage *image, int x, int y)
 {
+    if (x < 0 || y < 0 || x >= sfImage_getSize(image).x || y >= sfImage_getSize(image).y)
+        return sfColor_fromRGB(0, 0, 0);
     return sfImage_getPixel(image, x, y);
 }
