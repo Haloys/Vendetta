07d189609c28822fff6a07759375d0e72b30b5ea
Zooooral <maxence.bunel@epitech.eu>
2024-05-21 03:32:21 +0200

    feat: added new assets and fixed resources header

--- include/music.h
+++ include/music.h
@@ -10,14 +10,26 @@
 
 static const char *MUSIC_L[] = {
     "assets/musics/loading_music.ogg",
+    "assets/musics/first_map.ogg",
+    "assets/musics/second_map.ogg",
+    "assets/musics/third_map.ogg",
 };
 
 static const char *SOUND_L[] = {
-    "assets/sounds/player/punch.ogg",
-    "assets/sounds/enemies/pistol.ogg",
+    "assets/sounds/weapons/punch.ogg",
+    "assets/sounds/weapons/pistol.ogg",
     "assets/sounds/voice_over/first_book.ogg",
     "assets/sounds/voice_over/second_book.ogg",
     "assets/sounds/voice_over/third_book.ogg",
+    "assets/sounds/misc/chest.ogg",
+    "assets/sounds/misc/click_sound.ogg",
+    "assets/sounds/misc/footsteps.ogg",
+    "assets/sounds/weapons/knife.ogg",
+    "assets/sounds/weapons/minigun.ogg",
+    "assets/sounds/weapons/missile.ogg",
+    "assets/sounds/weapons/punch.ogg",
+    "assets/sounds/weapons/rifle.ogg",
+    "assets/sounds/weapons/shotgun.ogg",
 };
 
 #endif /* !MUSIC_H_ */
