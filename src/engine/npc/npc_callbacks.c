edd3e026968a92161443b374dd3216b5181343ac
Zooooral <maxence.bunel@epitech.eu>
2024-05-22 17:44:14 +0200

    fix: fixed the duplication bug with the npc's

--- src/engine/npc/npc_callbacks.c
+++ src/engine/npc/npc_callbacks.c
@@ -14,10 +14,15 @@
 
 int first_npc_callback(game_data_t *game, npc_t *npc)
 {
+    static int given = 0;
+
     printf("Interacting with %s\n", npc->config->name);
     game->player->npc_id = 0;
-    insert_item_in_inventory(game, "first_book", 1);
-    insert_item_in_inventory(game, "green_shoes", 1);
+    if (given == 0) {
+        insert_item_in_inventory(game, "first_book", 1);
+        insert_item_in_inventory(game, "green_shoes", 1);
+        given = 1;
+    }
     change_game_mode(game, NPC_DIALOGUE);
     return 0;
 }
@@ -25,11 +30,13 @@ int first_npc_callback(game_data_t *game, npc_t *npc)
 int second_npc_callback(game_data_t *game, npc_t *npc)
 {
     int qty = get_item_quantity(game, "goldcoin");
+    static int given = 0;
 
     printf("Interacting with %s\n", npc->config->name);
-    if (qty >= 1) {
+    if (qty >= 1 && given == 0) {
         remove_item_from_inventory_by_name(game, "goldcoin");
         insert_item_in_inventory(game, "rifle", 1);
+        given = 1;
     }
     game->player->npc_id = 1;
     change_game_mode(game, NPC_DIALOGUE);
@@ -38,8 +45,13 @@ int second_npc_callback(game_data_t *game, npc_t *npc)
 
 int third_npc_callback(game_data_t *game, npc_t *npc)
 {
+    static int given = 0;
+
     printf("Interacting with %s\n", npc->config->name);
-    insert_item_in_inventory(game, "oxycodone", 1);
+    if (given == 0) {
+        insert_item_in_inventory(game, "oxycodone", 1);
+        given = 1;
+    }
     game->player->npc_id = 2;
     change_game_mode(game, NPC_DIALOGUE);
     return 0;
@@ -47,18 +59,28 @@ int third_npc_callback(game_data_t *game, npc_t *npc)
 
 int fourth_npc_callback(game_data_t *game, npc_t *npc)
 {
+    static int given = 0;
+
     game->player->npc_id = 3;
     printf("Interacting with %s\n", npc->config->name);
-    insert_item_in_inventory(game, "second_book", 1);
-    insert_item_in_inventory(game, "keyc", 1);
+    if (given == 0) {
+        insert_item_in_inventory(game, "second_book", 1);
+        insert_item_in_inventory(game, "keyc", 1);
+        given = 1;
+    }
     change_game_mode(game, NPC_DIALOGUE);
     return 0;
 }
 
 int fifth_npc_callback(game_data_t *game, npc_t *npc)
 {
+    static int given = 0;
+
     printf("Interacting with %s\n", npc->config->name);
-    insert_item_in_inventory(game, "armor2", 1);
+    if (given == 0) {
+        insert_item_in_inventory(game, "armor2", 1);
+        given = 1;
+    }
     game->player->npc_id = 4;
     change_game_mode(game, NPC_DIALOGUE);
     return 0;
