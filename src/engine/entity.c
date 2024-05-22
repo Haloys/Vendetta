46ab3c60c4720b9783337e72bd0d1343f5a549b8
flav-code <flavien.de-rigne@epitech.eu>
2024-05-22 07:35:24 +0200

    feat: add function to detec if an entity is in an area

--- src/engine/entity.c
+++ src/engine/entity.c
@@ -12,3 +12,13 @@ rect_t get_hitbox_rect(sfVector2f *pos, int size)
 {
     return (rect_t){pos->x - size, pos->y - size, size * 2, size * 2};
 }
+
+bool is_inside_circle_area(sfVector2f *pos, sfVector2f pos2, float min_radius,
+    float max_radius)
+{
+    float distance = sqrt(pow(pos->x - pos2.x, 2) + pow(pos->y - pos2.y, 2));
+
+    if (distance < max_radius && distance > min_radius)
+        return true;
+    return false;
+}
