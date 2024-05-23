295ce085ff40b8faa63df7bb7906173611629a02
aloys <aloys.mangin@epitech.eu>
2024-05-23 20:42:02 +0200

    feat: add notification for npc

--- include/setup_notifications.h
+++ include/setup_notifications.h
@@ -12,21 +12,33 @@
 
 static const notification_params_t NOTIFICATION[] = {
     {"Permanent", "This will last indefinitely", SP_OBJECTIVE, -1},
-    {"Map Change", "Map change !", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Key A", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Key B", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Key C", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Knife", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Bandage", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Pistol", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Aidkit", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Rifle", SP_OBJECTIVE, 5.0},
-    {"New Item !", "You found Shotgun", SP_OBJECTIVE, 5.0},
-    {"Whaou !", "You found a chest", SP_OBJECTIVE, 5.0},
-    {"Incredible", "You found a secret chest", SP_OBJECTIVE, 5.0},
-    {"Amazing", "You found a secret chest", SP_OBJECTIVE, 5.0},
-    {"Unbelievable", "You found boss stash", SP_OBJECTIVE, 5.0},
-    {"Prodigious", "You found a massive stash", SP_OBJECTIVE, 5.0},
+    {"Map Change", "Map change !", SP_OBJECTIVE, 7.0},
+    {"New Item !", "You found Key A", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Key B", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Key C", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Knife", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Bandage", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Pistol", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Aidkit", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Rifle", SP_OBJECTIVE, 10.0},
+    {"New Item !", "You found Shotgun", SP_OBJECTIVE, 10.0},
+    {"Whaou !", "You found a chest", SP_OBJECTIVE, 10.0},
+    {"Incredible", "You found a secret chest", SP_OBJECTIVE, 10.0},
+    {"Amazing", "You found a secret chest", SP_OBJECTIVE, 10.0},
+    {"Unbelievable", "You found boss stash", SP_OBJECTIVE, 10.0},
+    {"Prodigious", "You found a massive stash", SP_OBJECTIVE, 10.0},
+    {"Oxycodone ?", "Trevor gave you some oxycodone, would\n"
+                "it be a good idea to take it?", SP_OBJECTIVE, 30.0},
+    {"New key !", "Oleksandra has given you a key! Will you,\n"
+        "go to the bottom of the cementery to,\n"
+            "stop this organisation!", SP_OBJECTIVE, 30.0},
+    {"Nice shoes !", "Mike gave you some nice green shoes !\n"
+        "Would  it be useful to equip them by,\n"
+            "opening the inventory?", SP_OBJECTIVE, 30.0},
+    {"That some protection !", "Mike has given you a suit of\n"
+        "armour that could be more than useful\n"
+            "for this vendetta!", SP_OBJECTIVE, 30.0},
+    {"Goldcoin", "You found a goldcoin", SP_OBJECTIVE, 30.0},
 };
 
 #endif /* SETUP_NOTIFICATIONS_H */
