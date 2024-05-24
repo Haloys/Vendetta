3188761d694d3dc5da26620d9aecbc8b05e8b7ce
flav-code <flavien.de-rigne@epitech.eu>
2024-05-24 03:45:39 +0200

    feat: player can't shoot if in passive mode

--- src/gameplay/events/movement.c
+++ src/gameplay/events/movement.c
@@ -137,7 +137,7 @@ static void check_bullet_player(game_data_t *game)
     sfVector2f dir = {0, 0};
     int damage = 1 + game->player->attack / 5;
 
-    if (game->clicked) {
+    if (game->clicked && game->is_passive == false) {
         angle = atan2f(game->mouse_pos.y - game->player->position.y,
             game->mouse_pos.x - game->player->position.x);
         dir = (sfVector2f){cosf(angle), sinf(angle)};
